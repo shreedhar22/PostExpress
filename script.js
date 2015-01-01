3ba8410380a9068484a899fce4762e58cc68691e
shreedharpawar <shreedhar22@gmail.com>
2015-01-01 08:08:27 -0800

    AddPost functionality using Angular Controller

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 angular.module("postApp",[])
-   
+   .controller("postController", ['$scope',function($scope){
+     $scope.posts=[];
+   $scope.post = {title:"",upvotes:0,link:"",comment:""}
+    $scope.title="";
+    $scope.upvotes = 0;
+    $scope.link="";
+    $scope.comment="";
+    
+    $scope.addPost=function(){
+    	
+    	$scope.post={title:$scope.title,upvotes:$scope.upvotes,link:$scope.link,comment:$scope.comment}
+        $scope.posts.push($scope.post)
+    };
  }])
 
 
