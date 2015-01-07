ba6253b45a7f6fe66d4e4da6e07f3c0ffaf99cd1
shreedharpawar <shreedhar22@gmail.com>
2015-01-07 14:33:34 -0800

    Angular Routing for viewing every post

--- script.js
+++ script.js
@@ -8,44 +8,61 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'HomeController'       //needs to be passed in the controller, we don't use it in html file after routing.
+      controller: 'HomeController'
     })
 
-     
+     .state('posts',{
+         url: '/posts/{id}',
+         templateUrl:'/posts.html',
+         controller:'PostCtrl'
+     });
+
   $urlRouterProvider.otherwise('home');
 }])
-
-   .factory('posts',[function(){           //'posts' is the argument passed in the controller where posts service is required.
+   
+   .factory('posts',[function(){
    	var o={
       posts:[]
    	};
    	  return o;
    }])
 
-   .controller("HomeController", ['$scope','posts',function($scope,posts){          //'posts' is the argument from the factory service. 
-     $scope.posts=posts.posts;                                                      // use of posts array form the 'posts' service
-   $scope.post = {title:"",upvotes:0,link:"",comment:""}
+  .controller('HomeController', ['$scope','posts',function($scope,posts){
+     $scope.posts=posts.posts;
+    $scope.post = {title:"",upvotes:0,link:"",comments:[]}
     $scope.title="";
     $scope.upvotes = 0;
     $scope.link="";
-    $scope.comment="";
+    $scope.comments=[];
+    $scope.comment='';
     
     $scope.addPost=function(){
-    	
-    	$scope.post={title:$scope.title,upvotes:$scope.upvotes,link:$scope.link,comment:$scope.comment}
+    	console.log("title=" + $scope.title);
+    	$scope.post={title:$scope.title,upvotes:$scope.upvotes,link:$scope.link,comment:[]}
         $scope.posts.push($scope.post)
-    };
-
+    }
 
     $scope.incrementVotes=function(index){
     	$scope.posts[index].upvotes=$scope.posts[index].upvotes+1
 
     }
 
+    $scope.addComment=function(index){
+          console.log($scope.comment + "lol");
+
+         $scope.posts[index].comment = $scope.comment;
+         
+    }
     $scope.removePost=function(index){
     	$scope.posts.splice(index,1)
     }
- }])
+  }])
+   
+   .controller('PostCtrl',['$scope','$stateParams','posts',function($scope,$stateParams,posts){
+        $scope.post=posts.posts[$stateParams.id]
+   }])
+
+
 
 
 
